44ceb6a23c0cd5960e2339b96f6748af6d276ae4
Jessie Contour <Contour@jessies-air.home>
2015-10-28 20:09:49 -0400

    update

--- views/js/main.js
+++ views/js/main.js
@@ -4,15 +4,22 @@ var app = app || {};
 app.main = (function() {
 
 	var parseData = function(trendArray, dataArray){
-		
+		//get arrays from app.js
+		$.get('/', function(res){
+
+		})
+		//loop if trend[i] == data[j].keywords[k]
+		//add data[j]._id to child of trend
 	}
 
 	var init = function(){
-		console.log('Initializing app.');	
+		console.log('Initializing app.');
+
 	};
 
 	return {
 		init: init,
+		parseData : parseData
 	};
 
 })();
