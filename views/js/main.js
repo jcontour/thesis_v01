9ba0732ca5c07bf4df2e034d107bfa48f049d10a
Jessie Contour <Contour@jessies-air.wireless.newschool.edu>
2015-10-31 14:43:44 -0400

    update

--- views/js/main.js
+++ views/js/main.js
@@ -4,8 +4,87 @@ var app = app || {};
 
 app.main = (function() {
 
+	var getNewChildren = function(id, callback){
+		$.post('/childData', {
+			'docid': id
+		}, function(res){
+			//WORKING ON THIS ONE RIGHT NOW
+			callback(json);
+		})
+	}
+
+	var pushArticleInfo = function(title, description){
+		$('#title').html('<p>' + title + '</p>');
+		$('#description').html('<p>' + description + '</p>');
+	}
+
+	var getArticleInfo = function(myId, callback){
+		$.post('/article', {
+			'docid': myId
+		}, function(res){
+			callback(res.title, res.description)
+		});
+
+	}
+
+	var listen = function(){
+		$('.article').click(function(){
+			getArticleInfo(this.id, function(title, description){
+				pushArticleInfo(title, description);
+				getNewChildren(this.id, function(json){
+					//make new articleBin with articles for children of this one
+				})
+			})
+		});
+	};
+
+	var createArticleBin = function(id){
+		var articleBin = $('<div>');
+		articleBin.addClass("articleBin").attr("id", id);
+		return articleBin;
+	}
+
+	var makeInitialElements = function(json){
+
+		var container = $('<div>');
+		container.attr("id", "container")
+		
+		var trendBin = $('<div>');
+		trendBin.addClass("articleBin").attr("id", "trend").html('<p>#NationalCatDay</p>')
+
+
+		var articleBin = $('<div>');
+		articleBin.addClass("articleBin").attr("id", "child1");
+
+		for (var i = 0; i < json.children.length; i ++){
+			var article = $('<div>');
+			article.addClass("article")
+					.html('article')
+					.attr("id", json.children[i])
+			articleBin.append(article);
+		}
+		
+		container.append(trendBin);
+		container.append(articleBin);
+		$('#viz').append(container)
+
+		listen();
+	}
+
+	var getData = function(callback){
+		var articles = {};
+		console.log("getting data..");
+		$.getJSON('/data', function(res){
+			articles = res;
+			callback(articles);
+		});
+	}
+
 	var init = function(){
 		console.log('Initializing app.');
+		getData(function(json){
+			makeInitialElements(json);
+		});
 	};
 
 	return {
--- views/js/visualization.js
+++ views/js/visualization.js
@@ -1,14 +1,19 @@
-var margin = {top: 20, right: 120, bottom: 20, left: 120},
+//the size of the svg container
+
+var margin = {top: 20, right: 50, bottom: 20, left: 50},
   width = 960 - margin.right - margin.left,
-  height = 500 - margin.top - margin.bottom;
+  height = 250 - margin.top - margin.bottom;
   
 var i = 0;
 
+//assigning variable to tree and setting size
 var tree = d3.layout.tree()
   .size([height, width]);
 
+//declaring the variable for drawing the lines
 var diagonal = d3.svg.diagonal()
   .projection(function(d) { return [d.y, d.x]; });
+
 
 var svg = d3.select("body").append("svg")
   .attr("width", width + margin.right + margin.left)
@@ -20,7 +25,11 @@ var svg = d3.select("body").append("svg")
 // d3.json("/data", function(error, treeData) {
 d3.json("js/data.json", function(error, treeData) {
   console.log(treeData);
+
+  //treeData[0] is top level of json file.
+  //to make different root, set something like treeData[0].children[#] etc
   root = treeData[0];
+  //this draws the diagram
   update(root);
 });
 
@@ -31,7 +40,8 @@ function update(source) {
     links = tree.links(nodes);
 
   // Normalize for fixed-depth.
-  nodes.forEach(function(d) { d.y = d.depth * 180; });
+  //horizontal distance away from previous level of graph
+  nodes.forEach(function(d) { d.y = d.depth * 200; });
 
   // Declare the nodes…
   var node = svg.selectAll("g.node")
@@ -40,6 +50,7 @@ function update(source) {
   // Enter the nodes.
   var nodeEnter = node.enter().append("g")
     .attr("class", "node")
+    .attr("id", function(d) { return d.name; })
     .attr("transform", function(d) { 
       return "translate(" + d.y + "," + d.x + ")"; });
 
@@ -47,14 +58,14 @@ function update(source) {
     .attr("r", 10)
     .style("fill", "#fff");
 
-  nodeEnter.append("text")
-    .attr("x", function(d) { 
-      return d.children || d._children ? -13 : 13; })
-    .attr("dy", ".35em")
-    .attr("text-anchor", function(d) { 
-      return d.children || d._children ? "end" : "start"; })
-    .text(function(d) { return d.name; })
-    .style("fill-opacity", 1);
+  // nodeEnter.append("text")
+  //   .attr("x", function(d) { 
+  //     return d.children || d._children ? -13 : 13; })
+  //   .attr("dy", ".35em")
+  //   .attr("text-anchor", function(d) { 
+  //     return d.children || d._children ? "end" : "start"; })
+  //   .text(function(d) { return d.name; })
+  //   .style("fill-opacity", 1);
 
   // Declare the links…
   var link = svg.selectAll("path.link")
@@ -65,4 +76,23 @@ function update(source) {
     .attr("class", "link")
     .attr("d", diagonal);
 
+click();
 }
+
+function click(){
+  var g = d3.select('body').selectAll('.node')
+  console.log(g);
+
+  g.on("click", function(){
+    d3.select(this)
+    .append('text').text(function(g) { 
+      
+
+      return g.name; 
+    })
+  });
+}
+
+//   g.on("mouseout", function(){})
+
+// }
